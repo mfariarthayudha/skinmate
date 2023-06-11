fabfb73038bfeae447845cbb93cece29e4bd0770
Salman Rausyan Fikri <salmanrausy@gmail.com>
2023-06-11 23:16:16 +0700

    mencoba menambahkan google authenticator firebase

--- app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/activity/login/LoginActivity.kt
@@ -7,9 +7,11 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -20,6 +22,9 @@ import com.dicoding.picodiploma.SkinMate.model.UserModel
 import com.dicoding.picodiploma.SkinMate.model.UserPreference
 import com.dicoding.picodiploma.SkinMate.view.ViewModelFactory
 import com.dicoding.picodiploma.SkinMate.view.ui.activity.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -27,11 +32,17 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
     private lateinit var user: UserModel
+    private lateinit var auth: FirebaseAuth
+// ...
+// Initialize Firebase Auth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
 
         setupView()
         setupViewModel()
@@ -67,35 +78,53 @@ class LoginActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
-            when {
-                email.isEmpty() -> {
-                    binding.emailEditTextLayout.error = "Masukkan email"
-                }
-                password.isEmpty() -> {
-                    binding.passwordEditTextLayout.error = "Masukkan password"
-                }
-                email != user.email -> {
-                    binding.emailEditTextLayout.error = "Email tidak sesuai"
-                }
-                password != user.password -> {
-                    binding.passwordEditTextLayout.error = "Password tidak sesuai"
-                }
-                else -> {
-                    loginViewModel.login()
-                    AlertDialog.Builder(this).apply {
-                        setTitle("Yeah!")
-                        setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
-                        setPositiveButton("Lanjut") { _, _ ->
-                            val intent = Intent(context, MainActivity::class.java)
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        val intent = Intent(this, MainActivity::class.java)
                             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                             startActivity(intent)
-                            finish()
-                        }
-                        create()
-                        show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            task.exception.toString(),
+                            Toast.LENGTH_SHORT,
+                        ).show()
                     }
                 }
-            }
+//            when {
+//                email.isEmpty() -> {
+//                    binding.emailEditTextLayout.error = "Masukkan email"
+//                }
+//                password.isEmpty() -> {
+//                    binding.passwordEditTextLayout.error = "Masukkan password"
+//                }
+//                email != user.email -> {
+//                    binding.emailEditTextLayout.error = "Email tidak sesuai"
+//                }
+//                password != user.password -> {
+//                    binding.passwordEditTextLayout.error = "Password tidak sesuai"
+//                }
+//                else -> {
+//                    loginViewModel.login()
+//                    AlertDialog.Builder(this).apply {
+//                        setTitle("Yeah!")
+//                        setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
+//                        setPositiveButton("Lanjut") { _, _ ->
+//                            val intent = Intent(context, MainActivity::class.java)
+//                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                            startActivity(intent)
+//                            finish()
+//                        }
+//                        create()
+//                        show()
+//                    }
+//                }
+//            }
         }
     }
 
