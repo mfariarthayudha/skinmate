9405a385e7644b9a51c34cfc900e1891bb36ee53
Salman Rausyan Fikri <salmanrausy@gmail.com>
2023-06-11 14:06:25 +0700

    menambahkan layout ke dalam fragment profile

--- app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/fragment/profile/ProfileFragment.kt
@@ -1,19 +1,23 @@
 package com.dicoding.picodiploma.SkinMate.view.ui.fragment.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.dicoding.picodiploma.SkinMate.R
 import com.dicoding.picodiploma.SkinMate.databinding.FragmentProfileBinding
+import com.dicoding.picodiploma.SkinMate.model.UserPreference
+import com.dicoding.picodiploma.SkinMate.view.ViewModelFactory
+import com.dicoding.picodiploma.SkinMate.view.ui.activity.welcome.WelcomeActivity
 
-class ProfileFragment : Fragment() {
+class ProfileFragment(private val dataStore: DataStore<Preferences>) : Fragment() {
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var profileViewModel: ProfileViewModel
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -21,17 +25,43 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
+//        val homeViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setUpViewModel()
+        setUpAction()
+
+//        val textView: TextView = binding.textProfile
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
+    }
+
+    private fun setUpAction() {
+        _binding!!.btnLogout.setOnClickListener{
+            profileViewModel.logout()
+        }
+    }
+
+    private fun setUpViewModel() {
+        profileViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore))
+        )[ProfileViewModel::class.java]
+
+        activity.let {
+            profileViewModel.getUser().observe(it!!) { user ->
+                if (user.isLogin) {
+                    binding.FullName.text = getString(R.string.greeting, user.name)
+                } else {
+                    startActivity(Intent(it, WelcomeActivity::class.java))
+                    requireActivity().finish()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/fragment/profile/ProfileViewModel.kt
+++ app/src/main/java/com/dicoding/picodiploma/SkinMate/view/ui/fragment/profile/ProfileViewModel.kt
@@ -3,10 +3,25 @@ package com.dicoding.picodiploma.SkinMate.view.ui.fragment.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.dicoding.picodiploma.SkinMate.model.UserModel
+import com.dicoding.picodiploma.SkinMate.model.UserPreference
+import kotlinx.coroutines.launch
 
-class ProfileViewModel : ViewModel(){
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+class ProfileViewModel(private val pref: UserPreference) : ViewModel(){
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is profile Fragment"
+//    }
+//    val text: LiveData<String> = _text
+
+    fun getUser(): LiveData<UserModel> {
+        return pref.getUser().asLiveData()
     }
-    val text: LiveData<String> = _text
+
+    fun logout() {
+        viewModelScope.launch {
+            pref.logout()
+        }
+    }
 }
